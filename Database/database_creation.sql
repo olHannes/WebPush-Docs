92306316f0c33e43e1f938f39a7c277f2e358059
Hannes Olschok <Hannes.o@t-online.de>
2025-10-27 16:47:21 +0100

    first discuss of database

--- Database/database_creation.sql
+++ Database/database_creation.sql
@@ -1,12 +1,3 @@
--- create enum: pi_group
-
--- create enum: notification_tags
-
--- create enum: notification_type
-
--- create enum: trigger_type
-
-
 -- create table: notification
 
 -- create table: notification_statistics

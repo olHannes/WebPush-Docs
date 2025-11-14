3323cf14b2174b417862f820b8a0042045fb2930
Hannes Olschok <hannes.o@t-online.de>
2025-11-14 08:31:55 +0100

    added example data for backend-jobs

---
+++ Database/Scripts/jobs_exampleDat.sql
@@ -0,0 +1,5 @@
+INSERT INTO smartmonitoring.datajobs (id, name, "desc", action, active, start, stop, repeatsecs) VALUES
+(1, 'getCheckForChange', '', 'CheckForChange', true, '2025-11-05 08:55:00.000', NULL, 5),
+(2, 'getMonthly', '', 'Monthly', true, '2025-11-05 08:55:00.000', NULL, NULL),
+(3, 'getScheduledTrigger', '', 'ScheduledTrigger', true, '2025-11-05 08:55:00.000', NULL, NULL),
+(4, 'getDatabasedTrigger', '', 'DatabasedTrigger', true, '2025-11-05 08:55:00.000', NULL, NULL);

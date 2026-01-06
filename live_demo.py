626008371650648948ce98c72bac86a61eb7af14
Alexander Nungesser <Alexander.Nungesser@wago.com>
2026-01-06 20:03:17 +0100

    fix live demo script

--- live_demo.py
+++ live_demo.py
@@ -2,12 +2,24 @@ import pandas as pd
 import requests
 import json
 
-CSV_PATH = "./sensor_b827eb0fae5c_202601051328.csv"
-POST_URL = "http://localhost:8080/SmartDataAirquality/smartdata/records/sensor_b827eb0fae5c?storage=smartmonitoring"
+PUT_URL = "http://localhost:8080/SmartDataAirquality/smartdata/records/group/9?storage=gamification"
+
+body = {"last_activity": "2023-08-29T08:27:41.497"}
+payload = json.dumps(body)
+
+# Send PUT request to change last_activity of group 9
+headers = {'Content-Type': 'application/json'}
+response = requests.put(PUT_URL, data=payload, headers=headers)
+print(f"Status code: {response.status_code}")
+print(f"Response: {response.text}")
+
+
+POST_URL_DATA = "http://localhost:8080/SmartDataAirquality/smartdata/records/sensor_b827eb0fae5c?storage=smartmonitoring"
+CSV_PATH_DATA = "./sensor_b827eb0fae5c_202601051328.csv"
 
 # Read CSV
 df = pd.read_csv(
-    CSV_PATH,
+    CSV_PATH_DATA,
     sep=",",
     quotechar='"',
     decimal='.',
@@ -17,15 +29,37 @@ df = pd.read_csv(
 # Convert DataFrame to list of dicts (records)
 data = df.to_dict(orient="records")
 
-# Build the custom JSON structure: list of dicts, then 'finished': true
-# payload = {"data": data.copy(), "finished": True}
+payload = json.dumps(data)
 
-# print("Prepared JSON data:")
-# print(json.dumps(payload))
-
-# Send POST request
+# Send POST request with sensor data
 headers = {'Content-Type': 'application/json'}
-response = requests.post(POST_URL, data=json.dumps(data), headers=headers)
+response = requests.post(POST_URL_DATA, data=payload, headers=headers)
+print(f"Status code: {response.status_code}")
+print(f"Response: {response.text}")
 
+POST_URL_TRAVEL_TYPES = "http://localhost:8080/SmartDataAirquality/smartdata/records/means_of_travel_join_process?storage=smartmonitoring"
+CSV_PATH_TRAVEL_TYPES = "./travel_type_data_for_sensor_b827eb0fae5c.csv"
+
+# Read CSV for travel types
+df_travel_types = pd.read_csv(
+    CSV_PATH_TRAVEL_TYPES,
+    sep=",",
+    quotechar='"'
+)
+# Convert DataFrame to list of dicts (records)
+data = df_travel_types.to_dict(orient="records")
+
+payload = json.dumps(data)
+
+# Send POST request with sensor data
+headers = {'Content-Type': 'application/json'}
+response = requests.post(POST_URL_TRAVEL_TYPES, data=payload, headers=headers)
+print(f"Status code: {response.status_code}")
+print(f"Response: {response.text}")
+
+
+# Send POST request to mark the data upload as finished
+headers = {'Content-Type': 'application/json'}
+response = requests.post(POST_URL_DATA, data=json.dumps({"finished": True}), headers=headers)
 print(f"Status code: {response.status_code}")
 print(f"Response: {response.text}")
